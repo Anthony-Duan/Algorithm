e685e21607c156b05b3868b365da12404bf7adc7
Anthony_Duan <1581041541@qq.com>
2018-07-28 22:44:47 +0800

    修改注释

--- SortingBasic/SelectionSort/main.java
+++ SortingBasic/SelectionSort/main.java
@@ -1,58 +1,27 @@
-package SortingBasic.InsertionSortAdvance;
-
-import SortingBasic.InsertionSort.SortTestHelper;
+package SortingBasic.InsertionSort;
 
 import java.util.Arrays;
 
 /**
- * @ Description:
- * @ Date: Created in 12:57 2018/7/27
+ * @ Description: 算法性能比较
+ * @ Date: Created in 12:32 2018/7/27
  * @ Author: Anthony_Duan
  */
 public class main {
-
-    // 比较SelectionSort和InsertionSort两种排序算法的性能效率
-    // 优化后，插入排序比选择排序性能略好
-    // 对于有序性强的数组，插入排序远远优于选择排序
     public static void main(String[] args) {
 
-        int N = 20000;
 
-        // 测试1 一般测试
+        // 比较SelectionSort和InsertionSort两种排序算法的性能效率
+        // 此时，插入排序比选择排序性能略低
+        int N = 20000;
         System.out.println("Test for random array, size = " + N + " , random range [0, " + N + "]");
 
         Integer[] arr1 = SortTestHelper.generateRandomArray(N, 0, N);
         Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
 
-        SortTestHelper.testSort("SortingBasic.InsertionSort.SelectionSort", arr1);
-        SortTestHelper.testSort("SortingBasic.InsertionSortAdvance.InsertionSort", arr2);
 
-        System.out.println();
-
-
-        // 测试2 有序性更强的测试
-        System.out.println("Test for more ordered random array, size = " + N + " , random range [0,3]");
-
-        arr1 = SortTestHelper.generateRandomArray(N, 0, 3);
-        arr2 = Arrays.copyOf(arr1, arr1.length);
-
-        SortTestHelper.testSort("SortingBasic.InsertionSort.SelectionSort", arr1);
-        SortTestHelper.testSort("SortingBasic.InsertionSortAdvance.InsertionSort", arr2);
-
-        System.out.println();
-
-
-        // 测试3 测试近乎有序的数组
-        int swapTimes = 100;
-        System.out.println("Test for nearly ordered array, size = " + N + " , swap time = " + swapTimes);
-
-        arr1 = SortTestHelper.generateNearlyOrderedArray(N, swapTimes);
-        arr2 = Arrays.copyOf(arr1, arr1.length);
-
-        SortTestHelper.testSort("SortingBasic.InsertionSort.SelectionSort", arr1);
-        SortTestHelper.testSort("SortingBasic.InsertionSortAdvance.InsertionSort", arr2);
-
+        SortTestHelper.testSort("SortingBasic.InsertionSortAdvance.SelectionSort", arr1);
+        SortTestHelper.testSort("SortingBasic.InsertionSort.InsertionSortAdvance", arr2);
         return;
     }
 }
-
